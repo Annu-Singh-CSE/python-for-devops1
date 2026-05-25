a30eced157d049877db74ba25172ee988e12481d
Annu-Singh-CSE <annusingh12112003@gmail.com>
2026-05-25 23:37:14 +0530

    second  commit

---
+++ casting.py
@@ -0,0 +1,10 @@
+"""var1=float(1000)
+print(type(var1))
+print(var1)"""
+#forcefully type casting
+num1="50"
+num2="20"
+res=int(num1)+int(num2)
+print(res)
+print(type(res))
+print(type(casting))S
--- typecasting.py
+++ typecasting.py
@@ -1,3 +1,11 @@
-n=10
-m=10.8
-print(n+m)#typecsting
+#typecasting
+#n=10
+#m=10.8
+#print(n+m)
+
+#concretation mtlb ek saath string aur nummber add hona
+num="28"
+name="annu"
+# print(num+" "+name)
+print(type(name))
+print(type(num))

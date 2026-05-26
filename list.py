b952cc61f74acd875a634fd231d1d502ea8de01c
Annu-Singh-CSE <annusingh12112003@gmail.com>
2026-05-26 10:44:34 +0530

    second commit

---
+++ list.py
@@ -0,0 +1,9 @@
+marks_10th=[20,90,78,90,"hello",5.5]
+print("before update : ",marks_10th)
+marks_10th[0]=200
+marks_10th[-1]=200
+i=8
+print(marks_10th[i]) # list index out of index
+print(len(marks_10th))
+print("after update : ", marks_10th)
+print("after update : ", marks_10th)
---
+++ student_name.py
@@ -0,0 +1,9 @@
+student_name="Annu-singh"
+student_address="noida sec15"
+student_number="8303000000"
+#student_profile="""hello my self annu,i am devops intern lookinf currently"""
+student_profile='kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk'
+print(len(student_number))
+print(student_name)
+print(student_address)
+print(student_profile)
